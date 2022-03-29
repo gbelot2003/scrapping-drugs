f72162e1083031328c50078b7170dc1e3fde8ded
Gerardo A Belot <gbelot2003@hotmail.com>
2022-03-28 23:07:33 -0600

    👌 UPDATE: updating handleFile

--- dist/misc/handleFiles.js
+++ dist/misc/handleFiles.js
@@ -1,21 +1,26 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.readFiles = exports.writeFiles = void 0;
 const fs_1 = require("fs");
-class handleFiles {
-    constructor(filepath, data) {
-        this.filePath = filepath;
-        this.localData = data;
-    }
-    WriteFile() {
-        (0, fs_1.writeFileSync)(this.filePath, JSON.stringify(this.localData));
-    }
-    ReadFile() {
-        let data;
-        let prelist;
-        let listArray;
-        data = (0, fs_1.readFileSync)(this.filePath, "utf8");
-        prelist = data.replace(/'/g, '"');
-        listArray = JSON.parse(prelist);
-        return listArray;
-    }
-}
+/** write files */
+const writeFiles = (filePath, data) => __awaiter(void 0, void 0, void 0, function* () {
+    yield (0, fs_1.writeFileSync)(filePath, JSON.stringify(data));
+});
+exports.writeFiles = writeFiles;
+/** readFIles files */
+const readFiles = (source) => __awaiter(void 0, void 0, void 0, function* () {
+    const data = (0, fs_1.readFileSync)(source, "utf8");
+    const prelistArray = data.replace(/g'/, '"');
+    const listArray = yield JSON.parse(prelistArray);
+    return listArray;
+});
+exports.readFiles = readFiles;
