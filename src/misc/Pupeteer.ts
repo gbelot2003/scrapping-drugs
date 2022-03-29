4f9e0a143e4a3fbfb65b9468f0f6c6cb392f7d66
Gerardo A Belot <gbelot2003@hotmail.com>
2022-03-29 09:49:08 -0600

    👎 NOT DONE: constructing the mock for popeteer

--- src/misc/Pupeteer.ts
+++ src/misc/Pupeteer.ts
@@ -1,4 +1,7 @@
 
+import puppeteer from 'puppeteer';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export class PupeteerCalls {
 
@@ -11,4 +14,22 @@ export class PupeteerCalls {
     public get getBaseUrl() : string {
         return this._baseUrl;
     } 
+
+    /**
+     *  firstCall
+     * @returns html
+     */
+         async firstCall(): Promise<any> {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(this.getBaseUrl);
+    
+            const html: any = await page.evaluate(() => {
+                return Array.from(document.querySelectorAll(".ddc-paging a")).map(x => x.getAttribute('href'));
+            });
+    
+            await browser.close();
+    
+            return html;
+        }
 }
--- test/unit/PuppeteerCalls.test.ts
+++ test/unit/PuppeteerCalls.test.ts
@@ -1,17 +1,27 @@
+import { launch } from "puppeteer";
 import { PupeteerCalls } from "../../src/misc/Pupeteer";
+import { stubBrowser } from "./mockPuppeteer";
 
 
 describe("Testing PuppeteerCalls class", () => {
     let instance: PupeteerCalls;
+    const baseUrl: string = (process.env["BASE_URL"] as string);
 
     beforeEach(() => {
-        const baseUrl: string = (process.env["BASE_URL"] as string);
         instance = new PupeteerCalls(baseUrl);
     });
 
     it('should get the baseUrl property', async () => {
         expect(instance).toBeInstanceOf(PupeteerCalls);
+        const url = await instance.getBaseUrl;
+        expect(url).toBe(baseUrl);
     });
+
+    jest.mock('puppeteer', () => ({
+        launch() {
+            return stubBrowser;
+        }
+    }));
 
 });
 
