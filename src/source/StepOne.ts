c2ce6aa6265008165734d1c2618b5dc29eacd68d
Gerardo A Belot <gbelot2003@hotmail.com>
2022-03-31 06:40:01 -0600

    👌 UPDATE: fully working

--- src/source/StepOne.ts
+++ src/source/StepOne.ts
@@ -3,5 +3,18 @@ import { HandleFiles } from '../misc/handleFiles';
 import { PupeteerCalls } from '../misc/PupeteerCalls';
 
 export class StepOne {
-    
+
+    public async execute(): Promise<any> {
+
+        console.log(chalk.yellow("Starting the scanning process,") + chalk.blue(" please wait..."));
+        const pupet = new PupeteerCalls();
+        const request: any = await pupet.firstCall();
+        console.log(request.html);
+
+        const handleFiles = new HandleFiles();
+
+        await console.log(chalk.yellow("About to create master list"));
+        await handleFiles.writeFiles("./downloads/masterlist.txt", request.html);
+        
+    }
 }
