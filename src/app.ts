71fca77147dc4b63315791111030ffcb971dd365
Gerardo A Belot <gbelot2003@hotmail.com>
2022-03-31 22:00:11 -0600

    👌 UPDATE: changing App from function to class

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
 import { StepOne } from './source/StepOne';
+import { StepThree } from './source/StepThree';
 import { StepTwo } from './source/StepTwo';
 
-async function main() {
-    const stepOne = new StepOne();
-    const stepTwo = new StepTwo();
 
-    await stepOne.execute();
-    await stepTwo.execute();
+class App {
+
+    private stepOne = new StepOne();
+    private stepTwo = new StepTwo();
+    private stepThree = new StepThree();
+    
+
+    async start() : Promise<void> {
+        try {
+            await this.stepOne.execute();
+            await this.stepTwo.execute();
+            await this.stepThree.execute();        
+            
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
-main();
+const main = new App();
+main.start();
